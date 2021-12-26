4936057f8dff43a8b9b89d82026c29461d40ec18
JVGS1111 <joaoviniciusgs@gmail.com>
2021-12-26 10:20:40 -0300

    Update GlobalStyles.ts

--- loja-de-jogos/src/global/GlobalStyles.ts
+++ loja-de-jogos/src/global/GlobalStyles.ts
@@ -2,18 +2,7 @@ import { darken } from "polished";
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-/* :root{
-        --background: #f0f2f5;
-        --red: #e52e4d;
-        --blue: #5429cc;
-        --green: #33cc95;
-        --blue-light:#6933ff;
 
-        --text-title:#363f5f;
-        --text-body: #969cb3;
-
-        --shape: #ffffff;
-    } */
 
     :root{
         --azul-escuro: #449dd1;
